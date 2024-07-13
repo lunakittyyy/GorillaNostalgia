25cc68e10ff6bf1c854de36a3ce4d9e9d9f69b3b
luna <17483712+lunakittyyy@users.noreply.github.com>
2024-07-12 22:31:55 -0400

    Initial commit

---
+++ GorillaNostalgia/Patches/CosmeticPatches.cs
@@ -0,0 +1,39 @@
+﻿using GorillaNetworking;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+
+namespace GorillaNostalgia.Patches
+{
+
+    public class CosmeticPatches
+    {
+        public static List<string> EACosmetics = new List<string> {
+            "LBAAE.",
+            "LFAAM.",
+            "LFAAN.",
+            "LHAAA.",
+            "LHAAK.",
+            "LHAAL.",
+            "LHAAM.",
+            "LHAAN.",
+            "LHAAO.",
+            "LHAAP.",
+            "LHABA.",
+            "LHABB."
+        };
+
+        [HarmonyPatch(typeof(VRRig))]
+        [HarmonyPatch("IsItemAllowed")]
+        public static class AllowedPatch {
+            public static bool Prefix(ref string itemName, ref bool __result)
+            {
+                if (EACosmetics.Contains(itemName) || !Plugin.useEarlyAccessCosmetics.Value)
+                {
+                    return true;
+                }
+                return false;
+            }
+        }
+    }
+}
---
+++ GorillaNostalgia/Plugin.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using System;
+using UnityEngine;
+using Utilla;
+
+namespace GorillaNostalgia
+{
+    [ModdedGamemode]
+    [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        bool inRoom;
+        public static ConfigEntry<bool> useEarlyAccessCosmetics;
+        public static ConfigEntry<bool> removeRocket;
+        public static ConfigEntry<bool> removeMouthAnimations;
+
+        void Start()
+        {
+            useEarlyAccessCosmetics = Config.Bind("Cosmetics", "UseEarlyAccessCosmetics", true, "Limit the cosmetics in the game to the ones in the Early Access DLC.");
+            removeRocket = Config.Bind("Players", "RemoveMouthAnimations", true, "Removes the speaking animations from player faces.");
+            removeRocket = Config.Bind("Maps", "RemoveRocket", true, "Removes the rocket from City.");
+            HarmonyPatches.ApplyHarmonyPatches();
+
+            if (removeRocket.Value)
+            {
+                Destroy(GameObject.Find("Environment Objects/LocalObjects_Prefab/City/CosmeticsRoomAnchor/RocketShip_IdleDummy"));
+                Destroy(GameObject.Find("Environment Objects/05Maze_PersistientObjects/RocketShip_Prefab"));
+            }
+        }
+    }
+}
